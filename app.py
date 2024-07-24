d58871bd731f02c056082a478c955cfe556ad533
mehdIDerrick <139765929+mehdIDerrick@users.noreply.github.com>
2024-07-24 14:52:10 +0100

    Update app.py data app.py main

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from typing import List, Optional
 import json
 import pandas as pd
 import uvicorn
-from datetime import datetime
 
 # Charger les données JSON depuis le fichier
 try:
@@ -25,11 +24,6 @@ class DataItem(BaseModel):
     entity_type_name: str
     entity_name: List[str]
 
-def safe_date_str(date):
-    if isinstance(date, datetime):
-        return date.isoformat()
-    return str(date)
-    
 # Instanciation de l'application FastAPI
 app = FastAPI()
 
@@ -123,17 +117,11 @@ async def get_data(
         raise HTTPException(status_code=404, detail="Aucune donnée ne correspond aux critères de filtrage")
     
     # Trier les données par "transaction_date" puis par "activation_date"
-    for item in filtered_data:
-        item["transaction_date"] = safe_date_str(item.get("transaction_date", ""))
-        item["activation_date"] = safe_date_str(item.get("activation_date", ""))
-
-    # Trier les données par "transaction_date" puis par "activation_date"
     sorted_data = sorted(
         filtered_data,
-        key=lambda x: (x["transaction_date"], x["activation_date"]),
+        key=lambda x: (x["trnsaction_date"], x["activation_date"]),
         reverse=True
     )
-
 
     # Ajouter le taux de conversion global à chaque ligne
     for item in sorted_data:
@@ -162,18 +150,13 @@ async def get_data_by_user(
 
     if not filtered_data:
         raise HTTPException(status_code=404, detail="Aucune donnée ne correspond aux critères de filtrage")
-
-    for item in filtered_data:
-        item["transaction_date"] = safe_date_str(item.get("transaction_date", ""))
-        item["activation_date"] = safe_date_str(item.get("activation_date", ""))
-
+    
     # Trier les données par "transaction_date" puis par "activation_date"
     sorted_data = sorted(
         filtered_data,
-        key=lambda x: (x["transaction_date"], x["activation_date"]),
+        key=lambda x: (x["trnsaction_date"], x["activation_date"]),
         reverse=True
     )
-
 
     # Ajouter le taux de conversion global à chaque ligne
     for item in sorted_data:
